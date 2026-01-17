0d84996899fd1be7127faae9945b34a046381d99
Your Name <mhmdmhfwzb57@gmail.com>
2026-01-17 17:26:02 +0200

    simple modal shown

---
+++ script.js
@@ -0,0 +1,34 @@
+//? BUGs: there few things u should be known like nodelist isnt like element its like array so u need to do the effect to inside of it
+//? or ur code will not excuted
+
+'use strict';
+
+let btnModel = document.querySelectorAll('.show-modal');
+let divModelsHidden = document.querySelectorAll('.hidden');
+let xBtn = document.querySelector('.close-modal');
+
+//! basic way to do it and take too much
+
+// btnModel.addEventListener('click', function () {
+//   for (let i = 0; i < divModel.length; i++) {
+//     divModel[i].style.display = 'block';
+//   }
+// });
+
+// xBtn.addEventListener('click', function () {
+//   for (let i = 0; i < divModel.length; i++) {
+//     divModel[i].style.display = 'none';
+//   }
+// });
+
+//? better way to do it and easier to read
+
+btnModel.forEach(e =>
+  e.addEventListener('click', function () {
+    divModelsHidden.forEach(e => e.classList.toggle('hidden'));
+  })
+);
+
+xBtn.addEventListener('click', function () {
+  divModelsHidden.forEach(e => e.classList.toggle('hidden'));
+});
