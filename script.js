b5231133fd9d7a9bdbd4d87c86fe32e397f85a23
Your Name <mhmdmhfwzb57@gmail.com>
2026-01-17 22:47:20 +0200

    update

--- script.js
+++ script.js
@@ -21,14 +21,34 @@ let xBtn = document.querySelector('.close-modal');
 //   }
 // });
 
-//? better way to do it and easier to read
+//* better way to do it and easier to read
 
-btnModel.forEach(e =>
-  e.addEventListener('click', function () {
-    divModelsHidden.forEach(e => e.classList.toggle('hidden'));
-  })
-);
+// btnModel.forEach(e =>
+//   e.addEventListener('click', function () {
+//     divModelsHidden.forEach(e => e.classList.toggle('hidden'));
+//   })
+// );
 
-xBtn.addEventListener('click', function () {
+// xBtn.addEventListener('click', function () {
+//   divModelsHidden.forEach(e => e.classList.toggle('hidden'));
+// });
+
+//* better way in read and in time of excuting
+
+let toggleFunction = function () {
   divModelsHidden.forEach(e => e.classList.toggle('hidden'));
-});
+};
+
+btnModel.forEach(e => e.addEventListener('click', toggleFunction));
+
+xBtn.addEventListener('click', toggleFunction);
+
+//* you will notice up here ⬆️ there is no forEach in xbtn bc we use same window for every modal
+//* when we have more xbtn we will use same method (forEach)
+
+//* keyboard function
+
+document.addEventListener(
+  'keydown',
+  e => e.key === 'Escape' && toggleFunction()
+);
